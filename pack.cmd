86c746b85abc70a99916589ecee0b90da46483a3
Atif Aziz <code@raboof.com>
2017-05-04 11:07:56 +0200

    Pack script arg doesn't need unquoting

--- pack.cmd
+++ pack.cmd
@@ -7,7 +7,7 @@ goto :EOF
 :main
 setlocal
 set VERSION_SUFFIX=
-if not "%~1"=="" set VERSION_SUFFIX=--version-suffix %~1
+if not "%~1"=="" set VERSION_SUFFIX=--version-suffix %1
 call build                                               ^
  && dotnet pack                                          ^
            --no-build --include-symbols --include-source ^
